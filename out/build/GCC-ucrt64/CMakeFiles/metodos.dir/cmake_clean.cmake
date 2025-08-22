527aacefc9419348162d2927a18b9f4fb71f88fd
Guidjy <guippimentel@gmail.com>
2025-08-22 13:34:31 -0300

    primeiro commit

---
+++ out/build/GCC-ucrt64/CMakeFiles/metodos.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/metodos.dir/src/main.c.obj"
+  "CMakeFiles/metodos.dir/src/main.c.obj.d"
+  "CMakeFiles/metodos.dir/src/metodos.c.obj"
+  "CMakeFiles/metodos.dir/src/metodos.c.obj.d"
+  "libmetodos.dll.a"
+  "metodos.exe"
+  "metodos.exe.manifest"
+  "metodos.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/metodos.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
